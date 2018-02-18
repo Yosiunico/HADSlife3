600f5d3ec12f56e070dae7fcc0fa597a8b50fa1b
FosterGun <fostergun123@gmail.com>
2018-02-18 16:53:46 +0100

    Servicio de correo eléctronico implementado Revisión pendiente, no es completamente funcional.

---
+++ EmailServices/EmailServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mail;
+
+namespace EmailServices
+{
+    public class EmailServices
+    {
+
+        public void EnviarEmail(string destinatario, string asunto, string mensaje)
+        {
+            MailMessage mail = new MailMessage();
+
+            mail.To = destinatario;
+            mail.From = "remitente@example.com"; //Aquí iría el correo con el que enviaremos los emails.
+            mail.BodyFormat = MailFormat.Html;  //Para enviar email con formato.
+            mail.Subject = asunto;
+            mail.Body = mensaje;
+
+            try
+            {
+                mail.Fields["http://schemas.microsoft.com/cdo/configuration/smtpauthenticate"] = 1;
+                mail.Fields["http://schemas.microsoft.com/cdo/configuration/sendusername"] = "usuario"; //Aquí iría el correo con el que enviaremos los emails.
+                mail.Fields["http://schemas.microsoft.com/cdo/configuration/sendpassword"] = "clave"; //Aquí iría la contraseña del correo con el que enviaremos los emails.
+                SmtpMail.SmtpServer = "smtpserver"; //Servidor smtp que se utilizará para enviar los emails.
+
+                SmtpMail.Send(mail);
+            } catch (Exception MailEx) {
+                throw new Exception(MailEx.Message);
+            }
+        }
+    }
+}
